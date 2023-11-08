a71213a161d07f8881057d23709587b9b9d35a00
Alex <alexandru.smochina.7e7@itb.cat>
2023-11-08 10:07:17 +0100

    Clase 2

---
+++ Script/Index.js
@@ -0,0 +1,31 @@
+function numeroAleatorioDeCincoDigitosConCeros() {
+    var numeroSecreto = Math.floor(10000 + Math.random() * 90000);
+    return numeroSecreto.toString().padStart(5, '0');
+}
+  
+  var numeroRandom = numeroAleatorioDeCincoDigitosConCeros();
+  console.log(numeroRandom);
+  
+
+function enviar(){
+
+    let numeros = document.getElementById("numero").value
+    if (numeros.length != 5){
+        window.alert("El numero esta fuera de rango");
+    }
+    else{
+        let numerosSeparados = numero.split("");
+    }
+
+    var tabla = document.getElementsByName("div")
+    var newRow = tabla.insertRow(tabla.rows.length)
+
+
+    for(let i=0; i < 5; i++){
+        var celda = newRow.insertCelda(i);
+        celda.innerHTML = "$numeros";
+
+    }
+}
+var botonAgregarFila = document.getElementsByClassName('enviar');
+botonAgregarFila.addEventListener('click', enviar);
