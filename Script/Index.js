16db03f15dc331cd12cfac54e5564f4b3671c564
Alex <alexandru.smochina.7e7@itb.cat>
2023-11-09 12:48:43 +0100

    Clases

--- Script/Index.js
+++ Script/Index.js
@@ -1,39 +1,45 @@
-
-var intentos = 0
+var intentos = 0;
 
 function numeroAleatorioDeCincoDigitosConCeros() {
     var numeroSecreto = Math.floor(10000 + Math.random() * 90000);
     return numeroSecreto.toString().padStart(5, '0');
 }
   
-  var numeroRandom = numeroAleatorioDeCincoDigitosConCeros();
-  console.log(numeroRandom);
+var numeroRandom = numeroAleatorioDeCincoDigitosConCeros();
+console.log(numeroRandom);
   
-  var digitos = numeroRandom.split('').map(Number);
+var digitos = numeroRandom.split('').map(Number);
 
 console.log(digitos); // Esto mostrará un array con cada dígito del número
 
-function enviar(){
+function enviar() {
+    let Numero = document.getElementById("numero");
+    let valor = Numero.value;
+    Numero.value = ""; // Corregido, cambié valor.value a Numero.value
 
-  let Numero = document.getElementById("numero");
-  let valor = Numero.value;
-  valor.value = ""
-  
-  if (/^\d{5}$/.test(valor)) {
-    console.log(valor)
-    var numeroSeparado = valor.split('').map(Number);
-    console.log(numeroSeparado)
-  } else {
-    alert("El valor debe contener exactamente 5 cifras.");
-  }
+    if (/^\d{5}$/.test(valor)) {
+        console.log(valor);
+        var numeroSeparado = valor.split('').map(Number);
+        console.log(numeroSeparado);
+    } else {
+        alert("El valor debe contener exactamente 5 cifras.");
+        return; // Salir de la función si la validación no pasa
+    }
 
-  for (i=0 ;i<5; i++){
-  var currentDiv = document.getElementById("cuadroNumeros");
-  console.log(typeof currentDiv);
-  var newDiv = document.createElement("div");
-  newDiv.textContent = numeroSeparado[i];
-  currentDiv.appendChild(newDiv);
-  newDiv.classList.add("numerosIntroducidos", "$numeroSeparado")
-  
-  }
+    for (i = 0; i < 5; i++) {
+        var currentDiv = document.getElementById("cuadroNumeros");
+        console.log(typeof currentDiv);
+        var newDiv = document.createElement("div");
+        newDiv.textContent = numeroSeparado[i];
+        currentDiv.appendChild(newDiv);
+        newDiv.classList.add("numerosIntroducidos"); // Corregido, eliminé el signo de dólar $
+
+        if (numeroSeparado[i] === digitos[i]) {
+            newDiv.style.backgroundColor = "green";
+        } else if (digitos.includes(numeroSeparado[i])) {
+            newDiv.style.backgroundColor = "yellow";
+        } else {
+            newDiv.style.backgroundColor = "grey";
+        }
+    }
 }
