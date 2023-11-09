c8f4e78e793b3c69d33f09e8d9c0d0b6002da9e2
Alex <alexandru.smochina.7e7@itb.cat>
2023-11-09 11:54:05 +0100

    Clase

--- Script/Index.js
+++ Script/Index.js
@@ -1,3 +1,6 @@
+
+var intentos = 0
+
 function numeroAleatorioDeCincoDigitosConCeros() {
     var numeroSecreto = Math.floor(10000 + Math.random() * 90000);
     return numeroSecreto.toString().padStart(5, '0');
@@ -17,11 +20,20 @@ function enviar(){
   valor.value = ""
   
   if (/^\d{5}$/.test(valor)) {
-    let numeroSeparado = valor.split('').map(Number);
+    console.log(valor)
+    var numeroSeparado = valor.split('').map(Number);
     console.log(numeroSeparado)
   } else {
     alert("El valor debe contener exactamente 5 cifras.");
   }
 
+  for (i=0 ;i<5; i++){
+  var currentDiv = document.getElementById("cuadroNumeros");
+  console.log(typeof currentDiv);
+  var newDiv = document.createElement("div");
+  newDiv.textContent = numeroSeparado[i];
+  currentDiv.appendChild(newDiv);
+  newDiv.classList.add("numerosIntroducidos", "$numeroSeparado")
   
+  }
 }
